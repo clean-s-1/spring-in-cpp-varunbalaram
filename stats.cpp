27cdec064e9a334b7c88f9bb7d586a0498cdbb18
varunbalaram <104979651+varunbalaram@users.noreply.github.com>
2022-05-09 22:06:05 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,21 +5,24 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& values ) {
     float sum = 0.0;
     float max = 0.0;
     float min = 0.0;
-    for (auto itr = values.begin(); itr != values.end(); itr++)
-    {
-        sum += *itr;
-        if (*itr > max)
-        {
-            max = *itr;
-        }
-        if (*itr < min)
-        {
-            min = *itr;
-        }
-    }
     Stats data;
-    data.average = sum / values.size();
-    data.max = max;
-    data.min = min;
+    if(!values.empty())
+    {
+        for (auto itr = values.begin(); itr != values.end(); itr++)
+        {
+            sum += *itr;
+            if (*itr > max)
+            {
+                max = *itr;
+            }
+            if (*itr < min)
+            {
+                min = *itr;
+            }
+        }
+        data.average = sum / values.size();
+        data.max = max;
+        data.min = min;
+    }
     return data;
 }
