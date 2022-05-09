cc522a1cd6a56bda88867dc47b9fdd4e55b7e5c4
varunbalaram <104979651+varunbalaram@users.noreply.github.com>
2022-05-09 22:34:14 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,4 +12,66 @@ namespace Statistics {
     };
     
     Stats ComputeStatistics(const std::vector<float>& );
+    
+    class IAlerter
+    {
+        public:
+        void SetAlert(bool value) = 0;
+    };
+    
+    class EmailAlert: public IAlerter
+    {
+    public:
+        EmailAlert():emailSent(false){}
+        void SetAlert(bool value)
+        {
+            emailSent = value;
+        }
+        bool emailSent;
+    };
+    
+    class LEDAlert: public IAlerter
+    {
+    public:
+        LEDAlert():ledGlows(false){}
+        void SetAlert(bool value)
+        {
+            ledGlows = value;
+        }
+        bool ledGlows;
+    };
+    
+    class StatsAlerter
+    {
+        private:
+        float maxLimit;
+        std::vector<IAlerter*> notifiers;
+        public:
+        StatsAlerter(float limit, std::vector<IAlerter*>& values):maxLimit(limit),notifiers(values){}
+        checkAndAlert(const std::vector<float>& values)
+        {
+            float max = 0.0;
+            bool alert = false;
+            if(!values.empty())
+            {
+                for (auto itr = values.begin(); itr != values.end(); itr++)
+                {
+                    if (*itr > max)
+                    {
+                        max = *itr;
+                    }
+                }
+                if(max > maxLimit)
+                {
+                    alert = true;
+                }
+                
+                for (auto itr = notifiers.begin(); itr != notifiers.end(); itr++)
+                {
+                    itr->SetAlert(alert);
+                }
+                
+            }
+        }
+    };
 }
