4c8c5a2bc802a6ebcb5c829968e23eba4aa18fa2
varunbalaram <104979651+varunbalaram@users.noreply.github.com>
2022-05-09 22:06:08 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,10 +1,11 @@
 #include <vector>
+#include <math.h>
 
 namespace Statistics {
     class Stats
     {
         public:
-        Stats():average(0.0),max(0.0),min(0.0){}
+        Stats():average(NAN),max(NAN),min(NAN){}
         float average;
         float max;
         float min;
